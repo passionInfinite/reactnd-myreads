369493f2f744446731ec6e32d545f6ca7bd3c8da
smirk <hardikpatel.hardik36@gmail.com>
2017-07-07 15:15:36 +0530

    refactor: add renderShelf method to render particular self instead of repeating

--- src/BookSelf.js
+++ src/BookSelf.js
@@ -2,6 +2,34 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class BookSelf extends React.Component {
+    renderShelf(shelfBooks){
+        return (
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {shelfBooks.map((book) => (
+                        <li key={book.id}>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                    <div className="book-shelf-changer">
+                                        <select value={book.shelf} id={book.id} onChange={this.props.onUpdateShelf.bind(this, book)}>
+                                            <option value="none" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors.join(", ")}</div>
+                            </div>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
     render(){
         return (
             <div className="list-books">
@@ -12,84 +40,15 @@ class BookSelf extends React.Component {
                     <div>
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    {this.props.currentlyReading.map((book) => (
-                                        <li key={book.id}>
-                                            <div className="book">
-                                                <div className="book-top">
-                                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                    <div className="book-shelf-changer">
-                                                        <select value={book.shelf} id={book.id} onChange={this.props.onUpdateShelf.bind(this, book)}>
-                                                            <option value="none" disabled>Move to...</option>
-                                                            <option value="currentlyReading">Currently Reading</option>
-                                                            <option value="wantToRead">Want to Read</option>
-                                                            <option value="read">Read</option>
-                                                            <option value="none">None</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div className="book-title">{book.title}</div>
-                                                <div className="book-authors">{book.authors.join(", ")}</div>
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ol>
-                            </div>
+                            {this.renderShelf(this.props.currentlyReading)}
                         </div>
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Want to Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    {this.props.wantToRead.map((book) => (
-                                        <li key={book.id}>
-                                            <div className="book">
-                                                <div className="book-top">
-                                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                    <div className="book-shelf-changer">
-                                                        <select value={book.shelf} id={book.id} onChange={this.props.onUpdateShelf.bind(this, book)}>
-                                                            <option value="none" disabled>Move to...</option>
-                                                            <option value="currentlyReading">Currently Reading</option>
-                                                            <option value="wantToRead">Want to Read</option>
-                                                            <option value="read">Read</option>
-                                                            <option value="none">None</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div className="book-title">{book.title}</div>
-                                                <div className="book-authors">{book.authors.join(", ")}</div>
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ol>
-                            </div>
+                            {this.renderShelf(this.props.wantToRead)}
                         </div>
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    {this.props.read.map((book) => (
-                                        <li key={book.id}>
-                                            <div className="book">
-                                                <div className="book-top">
-                                                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                    <div className="book-shelf-changer">
-                                                        <select value={book.shelf} id={book.id} onChange={this.props.onUpdateShelf.bind(this, book)}>
-                                                            <option value="none" disabled>Move to...</option>
-                                                            <option value="currentlyReading">Currently Reading</option>
-                                                            <option value="wantToRead">Want to Read</option>
-                                                            <option value="read">Read</option>
-                                                            <option value="none">None</option>
-                                                        </select>
-                                                    </div>
-                                                </div>
-                                                <div className="book-title">{book.title}</div>
-                                                <div className="book-authors">{book.authors.join(", ")}</div>
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ol>
-                            </div>
+                            {this.renderShelf(this.props.read)}
                         </div>
                     </div>
                 </div>
